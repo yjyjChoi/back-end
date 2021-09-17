ff8ba5d4f588c1a147a933fc2e799bdec6f09487
WooCheol <jikimee64@gmail.com>
2021-09-17 15:52:31 +0900

    [Feat] CloudFoundry 배포 환경설정 세팅, mysql -> mariadb 변경

--- src/main/java/com/pasta/aglioeolio/AglioEolioApplication.java
+++ src/main/java/com/pasta/aglioeolio/AglioEolioApplication.java
@@ -4,11 +4,12 @@ import java.util.TimeZone;
 import javax.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @SpringBootApplication
 @EnableAspectJAutoProxy
-public class AglioEolioApplication {
+public class AglioEolioApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AglioEolioApplication.class, args);
---
+++ src/main/java/com/pasta/aglioeolio/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.pasta.aglioeolio;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(AglioEolioApplication.class);
+    }
+
+}
